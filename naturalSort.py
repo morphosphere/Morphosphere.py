0a632a75807ed2382ed9bfbb11a79f89af05d944
lmurer <luca.murer@imls.uzh.ch>
2016-07-21 12:37:34 +0200

    Wrote basic function

--- naturalSort.py
+++ naturalSort.py
@@ -1,1 +1,21 @@
-#natural sort of string list arrays
+
+# coding: utf-8
+
+# In[16]:
+
+#! python2
+# naturalSort.py - natural sort of string list arrays
+
+from natsort import natsorted
+
+
+# In[17]:
+
+def naturalSort(unsortedList=range(11)):
+    if not isinstance(unsortedList, (list, tuple)):
+        print('Error: The input list is invalid')
+        return None
+    sortedList=natsorted(unsortedList)
+    
+    return sortedList
+
