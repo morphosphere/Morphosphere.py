48672a337fe06f0cbefddb87f50b5db4c2e6549a
lmurer <luca.murer@imls.uzh.ch>
2016-07-21 11:45:24 +0200

    Wrote basic function filterFileList()

--- filterFileList.py
+++ filterFileList.py
@@ -1,1 +1,34 @@
-#filter file list according to the provided regex
+
+# coding: utf-8
+
+# In[9]:
+
+#! python2
+# filterFileList.py - filter the filenameList according to a regex
+
+import re
+
+
+# In[23]:
+
+def filterFileList(fileList, regex=r'_(?P<siteName>s\d*)_(?P<channelName>w\d*).TIF'):
+    
+    # validate the regex
+    try:
+        regex=re.compile(regex)
+        is_valid=True
+        print('Filtering...')
+    except re.error:
+        print('The entered regex is invalid.')
+        is_valid=False
+        return None
+    
+    # filter the input list
+    filteredList=[]
+    for fileName in fileList:
+        if regex.search(fileName):
+            filteredList.append(fileName)
+    
+    print('Done.')
+    return filteredList
+
