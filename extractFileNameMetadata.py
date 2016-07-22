e4e9f2c0e724df3ce4ab7331321edb287b32be4f
lmurer <luca.murer@imls.uzh.ch>
2016-07-22 18:25:59 +0200

    defined a function extractFileNameMetadata() that returns a dictionary of the extracted information

---
+++ extractFileNameMetadata.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+
+# In[43]:
+
+# this script defines a function extractFileNameMetadata() that generates a
+# dictionary with the metadata extracted from a filename according
+# to a regular expression. Note that this works on single filename level.
+
+# import packages
+import re
+
+# expected format is .\Timepoint_1\20160126-corning-all-spheroids-p1-003hps_A01_w1.TIF
+
+def extractFileNameMetadata(fileName):
+    
+    # define the regex pattern
+    fileNamePattern = re.compile(r'Timepoint_(?P<timePoint>\d+)\\(?P<date>\d+)\-(?P<experimentName>.*)\-p(?P<plate>\d+)\-(?P<hps>\d+)hps_(?P<wellID>\w\d\d)_w(?P<channel>\d+)\.TIF', re.IGNORECASE)
+    
+    # produce a match object
+    match=fileNamePattern.search(fileName)
+    
+    # produce a dict of the match object
+    fileNameMetadata={}
+    fileNameMetadata=match.groupdict()
+    
+    return fileNameMetadata
+
